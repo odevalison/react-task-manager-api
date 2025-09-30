20a06da78bbc242128ca08f5ba32cfef0b3077a7
Alison G. Martins <odevalison@gmail.com>
2025-09-30 02:23:59 -0300

    fix: add write operations

--- api/server.ts
+++ api/server.ts
@@ -1,8 +1,14 @@
 import jsonServer from "json-server";
 import path from "path";
+import fs from "fs";
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(process.cwd(), "db.json"));
+
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
