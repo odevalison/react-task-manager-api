df5181ac1c0dd7db13b22de846c7e854207241b1
Alison G. Martins <odevalison@gmail.com>
2025-09-30 01:28:02 -0300

    feat: add typescript

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
+import jsonServer from "json-server";
+
+export const server = jsonServer.create();
 
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -8,5 +9,3 @@ server.use(middlewares);
 server.use(jsonServer.rewriter({ "/api/*": "/$1" }));
 server.use(router);
 server.listen(3000, () => console.log("server is running"));
-
-module.exports = server;
